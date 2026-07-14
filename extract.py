39d8e3cd85bd4506fabd8d185e1ec02ce08bfea2
Manitej Narayanadasu <mn7231f@gre.ac.uk>
2026-07-14 15:07:22 +0530

    Phase 1: PDF text extraction with line-merge cleanup

---
+++ extract.py
@@ -0,0 +1,19 @@
+import pdfplumber
+
+def extract_text_frm_pdf(path):
+    clean_lines = []
+    with pdfplumber.open(path) as sample_data:
+        for page in sample_data.pages:
+            info = page.extract_text()
+            if info is not None:
+                lines = info.split("\n")
+                for i in range(len(lines)):
+                    if lines[i].startswith(","):
+                        merged = clean_lines[-1] + lines[i]
+                        clean_lines.pop(-1)
+                        clean_lines.append(merged)
+                    else:
+                        clean_lines.append(lines[i])
+    final_output = "\n".join(clean_lines)
+    return final_output
+print(extract_text_frm_pdf("sample_data/sample_data_1.pdf"))
